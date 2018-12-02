e738901f7402d74d30e764fa977391072abe264b
Qisi Wang <qisi.wang@wisc.edu>
2018-12-01 22:24:01 -0600

    add visualization of the mesh

---
+++ VisualiseMesh.m
@@ -0,0 +1,42 @@
+function VisualiseMesh(nodes, els,strains,scheme,numbering)
+% visualize the 2d mesh
+% nodes, ele : structure for nodes and mesh
+% strains    : strain in each element
+% scheme     : 'e' - color by element numbering
+%              's' - color by strain
+%              'm' - color by material number
+% number     : 'n' - number nodes only
+%              'e' - number elements only
+%              'b' - number both
+%              no numbering otherwise
+
+% collect mesh info
+    nnode = size(nodes,1);
+    nel   = size(els,1);
+    nnpe  = size(els,2)-1;
+
+    X = zeros(nnpe,nel);
+    Y = zeros(nnpe,nel);
+    for i=1:nel
+        X(:,i) = nodes(els(i,2:end),1);
+        Y(:,i) = nodes(els(i,2:end),2);
+    end
+    if (scheme=='e')
+        C = 1:nel;
+    end
+        patch(X,Y,C);
+    if (numbering=='n'||numbering == 'b')
+        for i=1:nnode
+            text(nodes(i,1)-.1,nodes(i,2)-.1,num2str(i),'FontWeight','bold');
+        end
+    end
+    
+    if (numbering=='e'||numbering == 'b')
+        xe = sum(X,1)/nnpe;
+        ye = sum(Y,1)/nnpe;
+        for i=1:nel
+            text(xe(i),ye(i),num2str(i),'color','white');        
+        end
+    end
+
+end 
--- main.m
+++ main.m
@@ -1,27 +1,39 @@
 clear
 clc
+%% load data and visualize 
 filename = 'q9single';
 [nodes, els, mats, ndof, d] = parseInput(filename);
+VisualiseMesh(nodes, els(:,1:5),[],'e','b');
+hold off
 
-nnodes = size(nodes,1);
-nels   = size(els,1);
-nnpe   = size(els,2)-1;
-nmats  = size(mats,1);
-
-for i=1:nels
-    X = zeros(d,nnpe);
-    el = els(i,:);
-    for j=1:nnpe
-        X(:,j) = nodes(el(j+1),:)';
-    end
-    R = localSTF(X, mats(i,:));
-end
-
-T = importdata('tmat');
-dp2 = length(T)/18/18;
-T = reshape(T,[18,18,dp2]);
-W = importdata('wmat');
-W = reshape(W,[18,3]);
-% max(max(max(abs(R-T)./abs(R))))
-% eig(sum(R,3))
-% eig(sum(T,3))
+%% assemble stiffness matrix
+% 
+% nnodes = size(nodes,1);
+% nels   = size(els,1);
+% nnpe   = size(els,2)-1;
+% nmats  = size(mats,1);
+% 
+% K = sparse(nnodes*ndof,nnodes*ndof);
+% 
+% for i=1:nels
+%     X = zeros(d,nnpe);
+%     el = els(i,:);
+%     for j=1:nnpe
+%         X(:,j) = nodes(el(j+1),:)';
+%     end
+%     R = localSTF(X, mats(i,:)); 
+%     idx = [el(2:end)*2-1; el(2:end)*2];
+%     idx = reshape(idx,1,nnpe*ndof);
+%     symR = sum(R,3);
+%     symR = (symR+symR')/2;
+%     K(idx,idx) = K(idx,idx)+symR;
+% end
+% 
+% T = importdata('tmat');
+% dp2 = length(T)/18/18;
+% T = reshape(T,[18,18,dp2]);
+% W = importdata('wmat');
+% W = reshape(W,[18,3]);
+% % max(max(max(abs(R-T)./abs(R))))
+% % eig(sum(R,3))
+% % eig(sum(T,3))
